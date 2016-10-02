8b801d87ac1b80ef870cb628faf715adb562a6b9
Dan Lindeman <lindemda@gmail.com>
2016-10-02 19:38:20 -0400

    heroku support added

---
+++ kcards/views/index.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template
+
+
+blueprint = Blueprint('index', __name__)
+
+
+@blueprint.route("/")
+def index():
+    return render_template("base.html")
---
+++ manage.py
@@ -0,0 +1,31 @@
+#!env/bin/python
+
+import os
+
+from flask_script import Manager, Server
+
+from kcards.settings import get_config
+from kcards.app import create_app
+
+
+def find_assets():
+    """Yield paths for all static files and templates."""
+    for name in ['static', 'templates']:
+        directory = os.path.join(app.config['PATH'], name)
+        for entry in os.scandir(directory):
+            if entry.is_file():
+                yield entry.path
+
+
+config = get_config(os.getenv('FLASK_ENV'))
+
+app = create_app(config)
+
+server = Server(host='0.0.0.0', extra_files=find_assets())
+
+manager = Manager(app)
+manager.add_command('run', server)
+
+
+if __name__ == '__main__':
+    manager.run()
