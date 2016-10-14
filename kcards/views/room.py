ca4e3976a18f48bdac393e0a104ee623572ed23c
Dan Lindeman <lindemda@gmail.com>
2016-10-14 15:14:06 -0400

    update based on feedback, room.py uses correct comparison

--- kcards/views/room.py
+++ kcards/views/room.py
@@ -7,6 +7,7 @@ blueprint = Blueprint('room', __name__)
 @blueprint.route("/rooms/<key>")
 def detail(key):
     # Placeholder for how to 404 when a room is not in the room list.
-    if key is 'not_found':
-        key=None
+    # if key is None:
+    if key == 'not_found':
+        key = None
     return Response(render_template("room.html", key=key))
