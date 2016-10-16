4f27b2144df6771fc8f6ad427b840edf2453c568
Jace Browning <jacebrowning@gmail.com>
2016-10-16 13:00:58 -0400

    Connect the room view to the API

--- kcards/views/room.py
+++ kcards/views/room.py
@@ -1,4 +1,7 @@
 from flask import Blueprint, Response, render_template
+from flask_api import exceptions
+
+from . import api_rooms
 
 
 blueprint = Blueprint('room', __name__)
@@ -6,8 +9,9 @@ blueprint = Blueprint('room', __name__)
 
 @blueprint.route("/rooms/<code>")
 def detail(code):
-    # Placeholder for how to 404 when a room is not in the room list.
-    # if code is None:
-    if code == 'not_found':
-        code = None
-    return Response(render_template("room.html", room=code))
+    try:
+        room, _ = api_rooms.detail(code)
+    except exceptions.NotFound:
+        room = None
+
+    return Response(render_template("room.html", room=room))
