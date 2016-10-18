a6028de27008a252ad6de3c7230b58a17bf1fc18
Dan Lindeman <lindemda@gmail.com>
2016-10-18 00:21:00 -0400

    fixes for pylint

--- kcards/views/index.py
+++ kcards/views/index.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, Response, render_template, request, redirect, url_for
+from flask import Blueprint, Response, render_template, redirect, url_for
 
 from . import api_rooms
 
