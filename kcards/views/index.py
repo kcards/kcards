18f8a8f28f141ed21feaa03fa8944d29edf33a50
Dan Lindeman <lindemda@gmail.com>
2016-10-17 23:26:47 -0400

    initial drop

--- kcards/views/index.py
+++ kcards/views/index.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, Response, render_template
+from flask import Blueprint, Response, render_template, request
 
+from . import api_rooms
 
 blueprint = Blueprint('index', __name__)
 
@@ -7,3 +8,10 @@ blueprint = Blueprint('index', __name__)
 @blueprint.route("/")
 def index():
     return Response(render_template("index.html"))
+
+
+@blueprint.route("/", methods=['POST'])
+def create():
+    code = request.form['code']
+    content, _ = api_rooms.create()
+    # return Response(render_template("index.html"))
